f97e73c411fad9feab700c412285db40c5726259
mwatebelafabian88-eng <mwatebelafabian88@gmail.com>
2026-06-07 17:51:21 +0300

    Delete webhooks.js

--- api/webhooks.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    console.log('GitHub Ping received:', req.body)
-    return res.status(200).json({ received: true })
-  }
-  return res.status(405).end()
-}
